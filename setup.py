93ec3e0bcc61951068f7aabcc36b3005e09de4e6
martinbroede <martin.broede@yahoo.de>
2024-03-30 11:10:50 +0100

    specify requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import json
+import sys
 from pathlib import Path
 
 import setuptools
+
+if sys.version_info < (3, 9):
+    print("Python 3.9 or higher is required.")
+    sys.exit(1)
 
 PARENT_FOLDER = Path(__file__).parent
 
@@ -17,12 +22,17 @@ setuptools.setup(
     version=VERSION,
     author="Martin Broede",
     author_email="martin.broede@gmail.com",
+    maintainer="Martin Broede",
+    maintainer_email="martin.broede@gmail.com",
+    license="MIT",
     description="A lightweight Django app for viewing and managing log data conveniently",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/martinbroede/django-log-lens",
-    packages=setuptools.find_packages(),
+    packages=['django_log_lens'],
     include_package_data=True,
+    python_requires='>=3.9',
+    install_requires=["Django>=4.2"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -32,5 +42,4 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires='>=3.9',
 )
